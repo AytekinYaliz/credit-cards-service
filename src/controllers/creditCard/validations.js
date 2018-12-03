e53560fc87c07d011951153a331066bdd40796c5
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-12-03 21:03:39 +0000

    GREEN for invalid limit

--- src/controllers/creditCard/validations.js
+++ src/controllers/creditCard/validations.js
@@ -1,53 +1,55 @@
 // const validator = require('validator');
-const { isValidCardNumber } = require('../../libs/utilities');
+const { isValidCardNumber } = require("../../libs/utilities");
 
 const validations = {
    id: {
-      in: ['query'],
+      in: ["query"],
       custom: {
          options: (id, { req }) => isValidObjectId(req.params.id),
-         errorMessage: 'Wrong format!'
+         errorMessage: "Wrong format!"
       }
    },
    name: {
       // in: ['body'],
       isLength: {
          options: { min: 1 },
-         errorMessage: 'Name is required!'
+         errorMessage: "Name is required!"
       }
    },
    cardNumber: {
-      in: ['body'],
+      in: ["body"],
       isLength: {
          options: { min: 1, max: 19 },
-         errorMessage: 'Wrong format!'
+         errorMessage: "Wrong format!"
       },
       custom: {
-        options: (cardNumber, { req }) => isValidCardNumber(req.body.cardNumber),
-        errorMessage: "Invalid card number!"
+         options: (cardNumber, { req }) => isValidCardNumber(req.body.cardNumber),
+         errorMessage: "Invalid card number!"
       }
    },
-   // limi: {
-   //    in: ['body'],
-   //    isLength: {
-   //       options: { min: 1, max: 19 },
-   //       errorMessage: 'Wrong format!'
-   //    },
-   //    custom: {
-   //      options: (cardNumber, { req }) => isValidCardNumber(req.body.cardNumber),
-   //      errorMessage: "Invalid card number!"
-   //    }
-   // }
+   limit: {
+      in: ["body"],
+      isLength: {
+         options: { min: 1, max: 19 },
+         errorMessage: "Wrong format!"
+      },
+      custom: {
+         options: (cardNumber, { req }) => {
+            return /^\d+(\.\d{1,2})?$/.test(req.body.limit)
+         },
+         errorMessage: "Invalid limit!"
+      }
+   }
 };
 
 module.exports = {
-
    // POST /api/creditCards
    create: {
       name: {
-         in: ['body'],
+         in: ["body"],
          ...validations.name
       },
-      cardNumber: validations.cardNumber
+      cardNumber: validations.cardNumber,
+      limit: validations.limit
    }
 };
