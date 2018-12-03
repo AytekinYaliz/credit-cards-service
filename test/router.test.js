b880777a97b44dcac713a52088e9af655e120b07
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-12-03 16:41:58 +0000

    add fail test into /health-check endpoint

--- test/router.test.js
+++ test/router.test.js
@@ -4,12 +4,21 @@ const server = require('../src/server');
 
 
 describe('Health check..', () => {
-   it('should return OK for /api/health-check', (done) => {
+   it('should return OK for GET /api/health-check', (done) => {
       supertest(server)
          .get('/api/health-check')
          .end((err, res) => {
             expect(res.status).toBe(200);
             expect(res.text).toBe('OK');
+            done();
+         });
+   })
+
+   it('should return OK for POST /api/health-check', (done) => {
+      supertest(server)
+         .post('/api/health-check')
+         .end((err, res) => {
+            expect(res.status).toBe(404);
             done();
          });
    })
